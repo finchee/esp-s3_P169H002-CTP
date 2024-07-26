9c53ca22ec46c58606e83918a902c719834fdf7e
jiahui.li <jiahui.li@mixosense.com>
2024-07-26 23:09:24 +0800

    显示FPS

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <Arduino.h>
-#include "User_Setup.h"
 #include <TFT_eSPI.h>
+#include <lvgl.h>
+#include "User_Setup.h"
+#include "demos/lv_demos.h"
 
 TFT_eSPI tft = TFT_eSPI();
 
 // put function declarations here:
 void TFT_eSPI_setup(void);
+void get_FPS(void);
+void Screen_display(void);
+
+unsigned long start_millis = 0;
+int elapsed_time = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,7 +20,30 @@ void setup() {
 }
 
 void loop() {
+  get_FPS();
+  Screen_display();
+}
+
+// put function definitions here:
+void TFT_eSPI_setup(void) {
+
+  tft.init();
+  tft.setRotation(0);
+  tft.invertDisplay(1);
   
+  // 先初始化, 后亮屏可以避免开机花屏
+  ESP_LOGI("info","PWM start, TFT lighting...");
+  analogWrite(10, 127);
+}
+
+void get_FPS(void)
+{
+  elapsed_time = 1000 / (millis() - start_millis);
+  start_millis = millis();
+}
+
+void Screen_display(void)
+{
   // Fill screen with grey so we can see the effect of printing with and without 
   // a background colour defined
   tft.fillScreen(TFT_DARKGREY);
@@ -21,47 +51,15 @@ void loop() {
   // Set "cursor" at top left corner of display (0,0) and select font 2
   // (cursor will move to next line automatically during printing with 'tft.println'
   //  or stay on the line is there is room for the text with tft.print)
-  tft.setCursor(0, 0, 2);
+  tft.setCursor(16, 16, 2);
   // Set the font colour to be white with a black background, set text size multiplier to 1
-  tft.setTextColor(TFT_WHITE,TFT_BLACK);  tft.setTextSize(1);
+  tft.setTextColor(TFT_WHITE);  tft.setTextSize(1);
   // We can now plot text on screen using the "print" class
-  tft.println("Hello World!");
-  
-  // Set the font colour to be yellow with no background, set to font 7
-  tft.setTextColor(TFT_YELLOW,TFT_BLACK); tft.setTextFont(7);
-  tft.println(1234.56);
-  
-  // Set the font colour to be green with black background, set to font 4
-  tft.setTextColor(TFT_GREEN,TFT_BLACK);
-  tft.setTextFont(4);
-  tft.println("Groop");
-  tft.println("I implore thee,");
+  tft.println("This is a temple display FPS");
 
-  // Change to font 2
-  tft.setTextFont(2);
-  tft.println("my foonting turlingdromes.");
-  tft.println("And hooptiously drangle me");
-  tft.println("with crinkly bindlewurdles,");
-  // This next line is deliberately made too long for the display width to test
-  // automatic text wrapping onto the next line
-  tft.println("Or I will rend thee in the gobberwarts with my blurglecruncheon, see if I don't!");
   
-  // Test some print formatting functions
-  float fnumber = 123.45;
    // Set the font colour to be blue with no background, set to font 4
-  tft.setTextColor(TFT_BLUE);    tft.setTextFont(4);
-  tft.print("Float = "); tft.println(fnumber);           // Print floating point number
-  tft.print("Binary = "); tft.println((int)fnumber, BIN); // Print as integer value in binary
-  tft.print("Hexadecimal = "); tft.println((int)fnumber, HEX); // Print as integer number in Hexadecimal
-  delay(10000);
-}
-
-// put function definitions here:
-void TFT_eSPI_setup(void) {
-  ESP_LOGI("info","PWM start, TFT lighting...");
-  analogWrite(10, 32);
-
-  tft.init();
-  tft.setRotation(0);
-  tft.invertDisplay(1);
+  tft.setCursor(16, 32, 4);
+  tft.setTextColor(TFT_YELLOW);  tft.setTextSize(2);
+  tft.print("FPS:"); tft.println((int)elapsed_time, DEC); 
 }
